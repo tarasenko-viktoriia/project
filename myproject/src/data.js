e002c0e46382d9e0818c2c59eeb3f65e98ce6471
victoriataracenko <victoriataracenko0412@gmail.com>
2024-06-22 21:57:35 +0300

    1

---
+++ myproject/src/data.js
@@ -0,0 +1,68 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+	return [
+		{
+			name: "Beaver Creek",
+			cover:
+				"./images/images.jpg",
+			artist: "Aso, Middle School, Aviino",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: true,
+		},
+		{
+			name: "Daylight",
+			cover:
+				"./images/images2.jpg",
+			artist: "Aiguille",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Keep Going",
+			cover:
+				"./images/images3.jpg",
+			artist: "Swørn",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Nightfall",
+			cover:
+				"./images/images4.jpg",
+			artist: "Aiguille",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Reflection",
+			cover:
+				"./images/images5.jpg",
+			artist: "Swørn",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Under the City Stars",
+			cover:
+				"./images/images6.jpg",
+			artist: "Aso, Middle School, Aviino",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		//ADD MORE HERE
+	];
+}
+
+export default chillHop;
